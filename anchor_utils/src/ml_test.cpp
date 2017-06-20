075dab4a67eaffac1358d039f195f12b2830fc3c
Andreas Persson <andreas.persson@oru.se>
2017-06-20 15:19:00 +0200

    Minor changes.

--- anchor_utils/src/ml_test.cpp
+++ anchor_utils/src/ml_test.cpp
@@ -8,11 +8,25 @@ int main(int, char**) {
 
   // Read all samples from DB
   cv::Mat data, labels;
-  ml::read( "anchortmpdb", "dataset", data, labels);
-  std::cout<< "Samples: " << data.rows << "x" << data.cols << std::endl;
-  std::cout<< "Labels: " << labels.rows << "x" << labels.cols << std::endl;
+  ml::read( "anchorexp", "dataset", data, labels);
+  std::cout<< "Total samples: " << data.rows << "x" << data.cols << std::endl;
+  std::cout<< "Total Llabels: " << labels.rows << "x" << labels.cols << std::endl;
+  std::cout<< "---" << std::endl;
 
-  //void filter(const Mat &data, Mat &filtered, int idx);
+  // Split the data into traning and test data
+  cv::Mat trainData, trainLabels, testData, testLabels;
+  ml::split( data, labels, trainData, trainLabels, testData, testLabels);
+  std::cout<< "Traning samples: " << trainLabels.rows << std::endl;
+  std::cout<< "Test samples: " << testData.rows << std::endl;
+  std::cout<< "---" << std::endl;
 
-
+  // Filter the data
+  cv::Mat trainFiltered, testFiltered;
+  ml::filter( trainData, trainFiltered, 4);
+  ml::filter( testData, testFiltered, 4);
+  /*
+  std::cout<< "Train data filtered: " << trainFiltered.rows << "x" << trainFiltered.cols << std::endl;
+  std::cout<< "Test data filtered: " << testFiltered.rows << "x" << testFiltered.cols << std::endl;
+  std::cout<< "---" << std::endl;
+  */
 }
