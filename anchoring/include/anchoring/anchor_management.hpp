653c36994a19703b433f34b946071a406a11f369
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-03-01 21:20:39 +0100

    dc msg package fix

--- anchoring/include/anchoring/anchor_management.hpp
+++ anchoring/include/anchoring/anchor_management.hpp
@@ -1,13 +1,13 @@
 #ifndef __ANCHOR_MANAGEMENT_HPP__
-#define __ANCHOR_MANAGEMENT_HPP__ 
+#define __ANCHOR_MANAGEMENT_HPP__
 
-#include <anchor_msgs/ObjectArray.h> 
+#include <anchor_msgs/ObjectArray.h>
 #include <anchor_msgs/MovementArray.h>
-#include <anchor_msgs/TimedRequest.h> 
-#include <anchor_msgs/SpatialRequest.h> 
+#include <anchor_msgs/TimedRequest.h>
+#include <anchor_msgs/SpatialRequest.h>
 
-#include <dc_msgs/AssociationArray.h> 
-#include <dc_msgs/MeanPosHiddenArray.h> 
+#include <inference/AssociationArray.h>
+#include <inference/MeanPosHiddenArray.h> 
 
 #include <anchoring/anchor_container.hpp>
 #include <anchor_utils/ml.hpp>
@@ -18,16 +18,16 @@ using namespace std;
 // Class for creating and maintaining anchors
 // ------------------------------------------
 class AnchorManagement {
-  
+
   // An objects for holding all anchors
   std::unique_ptr<anchoring::AnchorContainer> _anchors;
 
-  // Object classicication 
-  machine::MachinePtr _classifier; 
-  
+  // Object classicication
+  machine::MachinePtr _classifier;
+
   /* --------------
      ROS variables
-     -------------- */  
+     -------------- */
   ros::NodeHandle _nh, _priv_nh;
   ros::Subscriber _object_sub, _track_sub, _assoc_sub;
   ros::ServiceServer _spatial_srv, _timed_srv;
@@ -71,7 +71,7 @@ class AnchorManagement {
 	}
       }
     }
-    
+
     // Get the id of the best matching anchor
     string getId() {
       string id = "";
@@ -85,13 +85,13 @@ class AnchorManagement {
       return id;
     }
   };
-  
+
   void match( const anchor_msgs::ObjectArrayConstPtr &object_ptr );
   float predict(map< string, map<anchoring::AttributeType, float> > &matches);
   void track( const dc_msgs::MeanPosHiddenArrayConstPtr &movement_ptr );
   void associate( const dc_msgs::AssociationArrayConstPtr &associations_ptr);
-  int process( map< string, map<anchoring::AttributeType, float> > &matches, 
-	       string &id, 
+  int process( map< string, map<anchoring::AttributeType, float> > &matches,
+	       string &id,
 	       float dist_th,
 	       float rate_th = 0.75 );
   bool spatialRequest( anchor_msgs::SpatialRequest::Request &req,
@@ -100,7 +100,7 @@ class AnchorManagement {
 		     anchor_msgs::TimedRequest::Response &res );
 
 
-public: 
+public:
   AnchorManagement(ros::NodeHandle nh);
   ~AnchorManagement() {}
   void init(int threads);
