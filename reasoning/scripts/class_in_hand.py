347ab5c19d58191343b2cd4ff117847ab9d0d4f1
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-07-10 11:22:09 +0200

    split up script file for each scenario

---
+++ reasoning/scripts/class_in_hand.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import os
+import sys
+
+
+import rospy
+import rospkg
+from pydc import DDC
+
+from anchor_msgs.msg import AnchorArray
+from anchor_msgs.msg import LogicAnchor
+from anchor_msgs.msg import LogicAnchorArray
+from anchor_msgs.msg import PositionAttribute
+
+
+
+
+class InHand():
+
+    def __init__(self, model_file, n_samples):
+        self.ddc = DDC(model_file, n_samples)
+        self.anchors_sub = rospy.Subscriber('anchors', AnchorArray, callback=self.process_anchors)
+        self.logic_anchors_publisher = rospy.Publisher('logic_anchors', LogicAnchorArray, queue_size=10)
+
+
+
+    def process_anchors(self, msg):
+        observations = self.make_observations(msg.anchors)
+        self.ddc.step(observations);
+
+        la_array = self.make_LogicAnchorArray(msg.anchors)
+        self.logic_anchors_publisher.publish(la_array)
+
+
+    def make_observations(self, anchors):
+        observations = []
+        for a in anchors:
+            obs = []
+
+            if self.filter(a):
+                position = a.position.data.pose.position
+                bbox = a.shape.data
+                color = a.color.symbols[0]
+                #TODO make probabilistic with prediciont socres
+                caffe = a.caffe.symbols[0]
+
+                obs.append("observation(anchor_r('{A_ID}'))~=({X},{Y},{Z})".format(A_ID=a.id, X=position.x, Y=position.y, Z=position.z))
+                obs.append("observation(anchor_bb('{A_ID}'))~=({BBX},{BBY},{BBZ})".format(A_ID=a.id, BBX=bbox.x, BBY=bbox.y, BBZ=bbox.z))
+                obs.append("observation(anchor_c('{A_ID}'))~={C}".format(A_ID=a.id, C=color))
+                obs.append("observation(anchor_caffe('{A_ID}'))~={Caffe}".format(A_ID=a.id, Caffe=caffe))
+                if self.is_hand(a):
+                    obs.append("observation(anchor_hand('{A_ID}'))~=true".format(A_ID=a.id))
+
+                obs = ','.join(obs)
+                observations.append(obs)
+        observations = ','.join(observations)
+        # print(observations)
+
+        return observations
+
+
+    def make_LogicAnchorArray(self, anchors_observed):
+        anchor_ids_observed = [a.id for a in anchors_observed]
+
+        la_array = LogicAnchorArray()
+
+        for a in anchors_observed:
+
+            la = LogicAnchor()
+
+            la.id = a.id
+
+            la.observed = True
+
+            la.color.symbols = a.color.symbols
+            la.color.predictions = a.color.predictions
+
+            particle_positions = self.ddc.querylist("(X,Y,Z)", "current(rv('{A_ID}'))~=(X,_,Y,_,Z,_)".format(A_ID=la.id))
+            for p in particle_positions:
+                x,y,z = p.split(",")
+                position = PositionAttribute()
+
+                position.data.pose.position.x = float(x)
+                position.data.pose.position.y = float(y)
+                position.data.pose.position.z = float(z)
+
+                la.particle_positions.append(position)
+
+
+            la_array.anchors.append(la)
+
+
+
+        anchor_ids = self.ddc.querylist("A_ID", "(current(hidden(A_ID,_)), current(anchor(A_ID)))")
+        anchor_ids = anchor_ids.keys()
+
+
+        for a_id in anchor_ids:
+            if a_id not in anchor_ids_observed:
+                la = LogicAnchor()
+                la.id = a_id
+
+                la.observed = False
+
+                color = self.ddc.querylist("Color", "(current(hidden(A_ID,_)), current(color('{A_ID}'))~=Color)".format(A_ID=la.id))
+                color = color.keys()
+                la.color.symbols = color
+                particle_positions = self.ddc.querylist("(X,Y,Z)", "(current(hidden(A_ID,_)),  current(hidden(A_ID,_)), current(rv('{A_ID}'))~=(X,_,Y,_,Z,_))".format(A_ID=la.id))
+                for p in particle_positions:
+                    x,y,z = p.split(",")
+                    position = PositionAttribute()
+                    position.data.pose.position.x = float(x)
+                    position.data.pose.position.y = float(y)
+                    position.data.pose.position.z = float(z)
+                    la.particle_positions.append(position)
+
+
+                la_array.anchors.append(la)
+
+                # print(la_array)
+                # caffe = self.ddc.querylist("Caffe","(current(caffe('{A_ID}'))~=Caffe)".format(A_ID=la.id))
+                # caffe = caffe.keys()
+                # print(caffe)
+                #
+                # hidden = self.ddc.query("current(hidden('{A_ID}',_))".format(A_ID=la.id))
+
+        print(len(la_array.anchors))
+        return la_array
+
+
+    def filter(self, anchor):
+        if "glasses" in anchor.caffe.symbols[0:4]:
+            return False
+        elif "banana" in anchor.caffe.symbols[0:1]:
+            return False
+        elif "skin" in anchor.caffe.symbols[0:2]:
+            return False
+        elif "squash" in anchor.caffe.symbols[0:2]:
+            return False
+        elif not anchor.caffe.symbols:
+            return False
+        else:
+            return True
+
+
+    def is_hand(self, anchor):
+        if "glove" in anchor.caffe.symbols[0:4]:
+            return True
+        else:
+            return False
--- reasoning/scripts/rel_track.py
+++ reasoning/scripts/rel_track.py
@@ -1,157 +1,44 @@
 #!/usr/bin/env python
 import os
+import sys
+
 
 import rospy
 import rospkg
-from pydc import DDC
-
-from anchor_msgs.msg import AnchorArray
-from anchor_msgs.msg import LogicAnchor
-from anchor_msgs.msg import LogicAnchorArray
-from anchor_msgs.msg import PositionAttribute
 
 
 
+def argparser():
+    import argparse
 
-class RelTrack():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name", type=str,
+                        help="name+path of this file")
 
-    def __init__(self, model_file, n_samples):
-        self.ddc = DDC(model_file, n_samples)
-        self.anchors_sub = rospy.Subscriber('anchors', AnchorArray, callback=self.process_anchors)
-        self.logic_anchors_publisher = rospy.Publisher('logic_anchors', LogicAnchorArray, queue_size=10)
+    parser.add_argument("-mode", type=str, help="which scenario to solve")
 
-
-
-    def process_anchors(self, msg):
-        observations = self.make_observations(msg.anchors)
-        self.ddc.step(observations);
-
-        la_array = self.make_LogicAnchorArray(msg.anchors)
-        self.logic_anchors_publisher.publish(la_array)
-
-
-    def make_observations(self, anchors):
-        observations = []
-        for a in anchors:
-            obs = []
-
-            if self.filter(a):
-                position = a.position.data.pose.position
-                bbox = a.shape.data
-                color = a.color.symbols[0]
-                #TODO make probabilistic with prediciont socres
-                caffe = a.caffe.symbols[0]
-
-                obs.append("observation(anchor_r('{A_ID}'))~=({X},{Y},{Z})".format(A_ID=a.id, X=position.x, Y=position.y, Z=position.z))
-                obs.append("observation(anchor_bb('{A_ID}'))~=({BBX},{BBY},{BBZ})".format(A_ID=a.id, BBX=bbox.x, BBY=bbox.y, BBZ=bbox.z))
-                obs.append("observation(anchor_c('{A_ID}'))~={C}".format(A_ID=a.id, C=color))
-                obs.append("observation(anchor_caffe('{A_ID}'))~={Caffe}".format(A_ID=a.id, Caffe=caffe))
-                if self.is_hand(a):
-                    obs.append("observation(anchor_hand('{A_ID}'))~=true".format(A_ID=a.id))
-
-                obs = ','.join(obs)
-                observations.append(obs)
-        observations = ','.join(observations)
-        # print(observations)
-
-        return observations
-
-
-    def make_LogicAnchorArray(self, anchors_observed):
-        anchor_ids_observed = [a.id for a in anchors_observed]
-
-        la_array = LogicAnchorArray()
-
-        # for a in anchors_observed:
-        #     la = LogicAnchor()
-        #
-        #     la.id = a.id
-        #
-        #     la.observed = True
-        #
-        #     la.color.symbols = a.color.symbols
-        #     la.color.predictions = a.color.predictions
-        #
-        #     particle_positions = self.ddc.querylist("(X,Y,Z)", "current(rv('{A_ID}'))~=(X,_,Y,_,Z,_)".format(A_ID=la.id))
-        #     for p in particle_positions:
-        #         x,y,z = p.split(",")
-        #         position = PositionAttribute()
-        #
-        #         position.data.pose.position.x = float(x)
-        #         position.data.pose.position.y = float(y)
-        #         position.data.pose.position.z = float(z)
-        #
-        #         la.particle_positions.append(position)
-        #
-        #
-        #     la_array.anchors.append(la)
-
-
-
-        anchor_ids = self.ddc.querylist("A_ID", "current(anchor(A_ID))")
-        anchor_ids = anchor_ids.keys()
-
-
-        for a_id in anchor_ids:
-            if a_id not in anchor_ids_observed:
-                la = LogicAnchor()
-                la.id = a_id
-
-                la.observed = False
-
-                color = self.ddc.querylist("Color", "current(color('{A_ID}'))~=Color".format(A_ID=la.id))
-                color = color.keys()
-                la.color.symbols = color
-                particle_positions = self.ddc.querylist("(X,Y,Z)", "current(rv('{A_ID}'))~=(X,_,Y,_,Z,_)".format(A_ID=la.id))
-                for p in particle_positions:
-                    x,y,z = p.split(",")
-                    position = PositionAttribute()
-                    position.data.pose.position.x = float(x)
-                    position.data.pose.position.y = float(y)
-                    position.data.pose.position.z = float(z)
-                    la.particle_positions.append(position)
-
-
-                la_array.anchors.append(la)
-
-                # print(la_array)
-                # caffe = self.ddc.querylist("Caffe","(current(caffe('{A_ID}'))~=Caffe)".format(A_ID=la.id))
-                # caffe = caffe.keys()
-                # print(caffe)
-                #
-                # hidden = self.ddc.query("current(hidden('{A_ID}',_))".format(A_ID=la.id))
-
-        return la_array
-
-
-    def filter(self, anchor):
-        if "glasses" in anchor.caffe.symbols[0:4]:
-            return False
-        elif "banana" in anchor.caffe.symbols[0:1]:
-            return False
-        elif "skin" in anchor.caffe.symbols[0:2]:
-            return False
-        elif "squash" in anchor.caffe.symbols[0:2]:
-            return False
-        elif not anchor.caffe.symbols:
-            return False
-        else:
-            return True
-
-
-    def is_hand(self, anchor):
-        if "glove" in anchor.caffe.symbols[0:4]:
-            return True
-        else:
-            return False
-
+    return parser
 
 if __name__ == "__main__":
     rospy.init_node("rel_track_node")
     path = rospkg.RosPack().get_path('reasoning')
-    model_file = os.path.join(path, 'models/behind_of.pl')
     N_SAMPLES = 200
 
-    rel_track = RelTrack(model_file, N_SAMPLES)
+    python_argv = rospy.myargv(argv=sys.argv)
+    parser = argparser()
+    args = vars(parser.parse_args(python_argv))
+
+    if args['mode']=="shellgame":
+        from class_shellgame import Shellgame
+        model_file = os.path.join(path, 'models/shellgame.pl')
+        rel_track = Shellgame(model_file, N_SAMPLES)
+    elif args['mode']=="behind_of":
+        from class_behind_of import BehindOf
+        model_file = os.path.join(path, 'models/behind_of.pl')
+        rel_track = BehindOf(model_file, N_SAMPLES)
+    elif args['mode']=="in_hand":
+        from class_in_hand import InHand
+        model_file = os.path.join(path, 'models/in_hand.pl')
+        rel_track = InHand(model_file, N_SAMPLES)
 
     rospy.spin()
