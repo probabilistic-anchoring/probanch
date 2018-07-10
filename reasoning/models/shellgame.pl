0d41bbec87bfd2e9a8a08ffb19ec7c396ac781cb
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2018-07-10 15:57:33 +0200

    shellgame.pl minor change

--- reasoning/models/shellgame.pl
+++ reasoning/models/shellgame.pl
@@ -29,16 +29,19 @@ cov(1,[Cov,0,0,0.000001],VarQ) :-
 	deltaT(DeltaT),
 	Cov is VarQ*DeltaT^2.
 
+observed_list:t+1 ~ val(A_List) <-
+   findall_forward(A_ID, (observation(anchor_r(A_ID))~=_), A_List).
+
+observed(A_ID):t+1 <-
+   observed_list:t+1 ~= A_List,
+   member(A_ID,A_List).
+
 
 box(A_ID):t+1 <-
-   box(A_ID):t,
-   observation(anchor_caffe(A_ID))~=C.
-box(A_ID):t+1 <-
-   observation(anchor_caffe(A_ID))~=box,
+   observation(anchor_r(A_ID))~=_,
    \+box(A_ID):t.
 box(A_ID):t+1 <-
-   observation(anchor_caffe(A_ID))~=block,
-   \+box(A_ID):t.
+   box(A_ID):t.
 
 small_box(A_ID):t+1 <-
    small_box(A_ID):t.
@@ -48,26 +51,13 @@ small_box(A_ID):t+1 <-
    C==green.
 
 big_box(A_ID):t+1 <-
-   small_box(A_ID):t.
+   big_box(A_ID):t.
 big_box(A_ID):t+1 <-
    box(A_ID):t+1,
    observation(anchor_c(A_ID))~=C,
    C==pink.
 
 
-observed_list:t+1 ~ val(A_List) <-
-   findall_forward(A_ID, (observation(anchor_r(A_ID))~=_), A_List).
-
-observed(A_ID):t+1 <-
-   observed_list:t+1 ~= A_List,
-   member(A_ID,A_List).
-
-
-anchor(A_ID):t+1 <-
-	observed(A_ID):t+1.
-anchor(A_ID):t+1 <-
-	anchor(A_ID):t,
-   hidden(A_ID,_):t+1.
 
 
 hidden(A_ID,A_ID_BigBox):t+1 <-
@@ -79,23 +69,18 @@ under_of(A_ID,A_ID_BigBox):t+1 <-
    observed(A_ID):t,
    \+observed(A_ID):t+1,
    pick_BigBox(A_ID):t+1 ~= A_ID_BigBox.
-% in_hand(A_ID,A_ID_hand):t+1 <-
-%    anchor(A_ID):t,
-%    anchor(A_ID_hand):t,
-%    in_hand(A_ID,A_ID_hand):t,
-%    \+observed(A_ID):t+1,
-%    anchor(A_ID_hand):t+1.
+under_of(A_ID,A_ID_BigBox):t+1 <-
+   small_box(A_ID):t+1,
+   under_of(A_ID,A_ID_BigBox):t,
+   \+observed(A_ID):t+1.
+
 pick_BigBox(A_ID):t+1 ~ uniform(BigBoxes) <-
    small_box(A_ID):t+1,
    observed(A_ID):t,
    \+observed(A_ID):t+1,
    \+hidden(A_ID,_):t,
    rv(A_ID):t ~= (X1,_,Y1,_,Z1,_),
-   % findall_forward(H, (big_box(BB):t+1, rv(BB):t+1~=(XBB,_,YBB,_,ZBB,_), D is sqrt((X1-XBB)^2+(Y1-YBB)^2), D<0.9), BigBoxes),
-   findall_forward(BB, (big_box(BB):t+1, rv(BB):t+1~=(XBB,_,YBB,_,ZBB,_)), BigBoxes),
-
-   writeln(BigBoxes),
-   false,
+   findall_forward(BB, (big_box(BB):t+1, rv(BB):t+1~=(XBB,_,YBB,_,ZBB,_),  D is sqrt((X1-XBB)^2+(Y1-YBB)^2), D<0.12), BigBoxes),
    \+BigBoxes=[].
 
 
@@ -104,106 +89,58 @@ pick_BigBox(A_ID):t+1 ~ uniform(BigBoxes) <-
 
 % observation position
 observation(anchor_r(A_ID)):t+1 ~ val(_) <-
-	anchor(A_ID):t+1,
-   \+anchor(A_ID):t.
-observation(anchor_r(A_ID)):t+1 ~ logfinite([W:_]) <-
-   anchor(A_ID):t,
-	anchor(A_ID):t+1,
-	rvProposal(_,A_ID):t+1 ~= [_|W].
+	box(A_ID):t+1.
 observation(anchor_r(A_ID)):t+1 ~ val(_) <- %not good
    anchor(_):t,
-   color(A_ID):t ~=C,
 	writeln('fuck leak'),
 	true.
 
+
+
+
 %observation color
 observation(anchor_c(A_ID)):t+1 ~ val(_) <-
-	anchor(A_ID):t+1.
+   true.
 %observation caffe
 observation(anchor_caffe(A_ID)):t+1 ~ val(_) <-
-	anchor(A_ID):t+1.
+   true.
 %observation bb
 observation(anchor_bb(A_ID)):t+1 ~ val(_) <-
-	anchor(A_ID):t+1.
-%observation hand
-observation(anchor_hand(A_ID)):t+1 ~ val(_) <-
-	anchor(A_ID):t+1.
+   true.
+
 
 
 
 %position transition
 rv(A_ID):t+1 ~  indepGaussians([ ([X,0],Cov), ([Y,0],Cov), ([Z,0],Cov) ]) <-
 	observation(anchor_r(A_ID)) ~= (X,Y,Z),
-	\+anchor(A_ID):t,
-	anchor(A_ID):t+1,
+	\+box(A_ID):t,
+	box(A_ID):t+1,
 	varQ(VarQ),
 	VarQ1 is VarQ/5,
 	cov(1,Cov,VarQ1).
 
+%replace this one with optimal proposal?
 rv(A_ID):t+1 ~  indepGaussians([ ([X,0],Cov), ([Y,0],Cov), ([Z,0],Cov) ]) <-
 	observation(anchor_r(A_ID)) ~= (X,Y,Z),
-	big_box(A_ID):t+1,
+   box(A_ID):t,
+	box(A_ID):t+1,
 	varQ(VarQ),
 	VarQ1 is VarQ/5,
 	cov(1,Cov,VarQ1).
 
-rv(A_ID):t+1 ~ val(V) <-
-	anchor(A_ID):t,
-   observation(anchor_r(A_ID))~=_,
-	anchor(A_ID):t+1,
-	rvProposal('observed',A_ID):t+1 ~= [V|_].
 
 rv(A_ID):t+1 ~ val(RV) <-
-   rv(A_ID):t ~= RV,
    big_box(A_ID):t+1,
-   \+observed(A_ID):t+1.
+	\+observed(A_ID):t+1,
+   rv(A_ID):t ~= RV.
 
 
-rv(A_ID):t+1 ~ val(V) <-
-   anchor(A_ID):t,
-   anchor(A_ID):t+1,
+rv(A_ID):t+1 ~ val(RV) <-
+   small_box(A_ID):t+1,
+   \+observation(anchor_r(A_ID)) ~= _,
    under_of(A_ID,A_ID_BigBox):t+1,
-	rvProposal('under_of',A_ID_BigBox):t+1 ~= [V|_].
-
-
-
-
-
-
-
-% directly observed
-rvProposal('observed',A_ID):t+1 ~ logIndepOptimalProposals([
-			([R_x_new,V_x_new],Cov, [1,0],[0.0001],[O_x]),
-			([R_y_new,V_y_new],Cov, [1,0],[0.0001],[O_y]),
-			([R_z_new,V_z_new],Cov, [1,0],[0.0001],[O_z])]) <-
-	observation(anchor_r(A_ID)) ~= (O_x,O_y,O_z),
-	rv(A_ID):t ~= (R_x,V_x,R_y,V_y,R_z,V_z),
-	varQ(VarQ),
-	cov(2,Cov,VarQ),
-	deltaT(DeltaT),
-	V_x_new is V_x,
-	V_y_new is V_y,
-	V_z_new is V_z,
-	R_x_new is R_x+DeltaT*V_x,
-	R_y_new is R_y+DeltaT*V_y,
-	R_z_new is R_z+DeltaT*V_z.
-
-% regenarte samples for observed anchor for the hidden anchor
-rvProposal('under_of',A_ID_BigBox):t+1 ~ logIndepOptimalProposals([
-			([R_x_new,V_x_new],Cov, [1,0],[0.0001],[O_x]),
-			([R_y_new,V_y_new],Cov, [1,0],[0.0001],[O_y]),
-			([R_z_new,V_z_new],Cov, [1,0],[0.0001],[O_z])]) <-
-	observation(anchor_r(A_ID_BigBox)) ~= (O_x,O_y,O_z),
-	rv(A_ID_BigBox):t ~= (R_x,V_x,R_y,V_y,R_z,V_z),
-	varQ(VarQ),
-	cov(2,Cov,VarQ),
-	deltaT(DeltaT),
-	V_x_new is V_x,
-	V_y_new is V_y,
-	V_z_new is V_z,
-	R_x_new is R_x+DeltaT*V_x,
-	R_y_new is R_y+DeltaT*V_y,
-	R_z_new is R_z+DeltaT*V_z.
+   rv(A_ID_BigBox):t+1 ~= RV.
 
 
 
@@ -211,23 +148,13 @@ rvProposal('under_of',A_ID_BigBox):t+1 ~ logIndepOptimalProposals([
 
 
 color(A_ID):t+1 ~ val(C) <-
-	anchor(A_ID):t+1,
-   \+anchor(A_ID):t,
 	observation(anchor_c(A_ID)) ~= C.
 color(A_ID):t+1 ~ val(C) <-
-	anchor(A_ID):t+1,
+	box(A_ID):t+1,
 	color(A_ID):t ~= C.
 
 caffe(A_ID):t+1 ~ val(Caffe) <-
-	anchor(A_ID):t+1,
 	observation(anchor_caffe(A_ID)) ~= Caffe.
 caffe(A_ID):t+1 ~ val(Caffe) <-
-	anchor(A_ID):t+1,
+	box(A_ID):t+1,
 	caffe(A_ID):t ~= Caffe.
-
-bb(A_ID):t+1 ~ val(BB) <-
-	anchor(A_ID):t+1,
-	observation(anchor_bb(A_ID)) ~= BB.
-bb(A_ID):t+1 ~ val(BB) <-
-	anchor(A_ID):t+1,
-	bb(A_ID):t ~= BB.
